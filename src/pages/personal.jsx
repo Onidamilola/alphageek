45d2e158534b5d19e2fc1cbb5d5e053fd08c9f29
Joy Onweani <joyamaka1995@gmail.com>
2024-03-08 10:42:10 +0100

    sessionstorage storing of personal payloads

--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -1,32 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { GET_EDUCATIONS } from '../utils/constant';
-import { useGlobalContext } from '../custom-hooks/Context';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { GET_EDUCATIONS } from "../utils/constant";
+// import { useGlobalContext } from "../custom-hooks/Context";
 
 const Personal = ({ nextStep }) => {
-  const [educations, setEducations] = useState([])
-  const {firstname, setFirstname, setMiddlename, setLastname, setAddress, setPhoneNum, setGender, setEducation} = useGlobalContext()
+  const [educations, setEducations] = useState([]);
 
-  const [firname, setFirname] = useState('')
-  const [midname, setMidname] = useState('')
-  const [lstname, setLstname] = useState('')
-  const [addr, setAddr] = useState('')
-  const [phnnum, setPhnnum] = useState('')
-  const [gendr, setGendr] = useState('')
-  const [edu, setEdu] = useState('')
-
-
+  const [personalList, setPersonalList] = useState({
+    firname: '',
+    midname: '',
+    lstname: '',
+    addr: '',
+    phnnum: '',
+    gendr: '',
+    edu: ''
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // You can perform additional validation here if needed
-    nextStep();
   };
 
   useEffect(() => {
     const fetchEducations = async () => {
       try {
-        const response = await axios.get( GET_EDUCATIONS );
+        const response = await axios.get(GET_EDUCATIONS);
         console.log("Educations Data:", response.data.data);
         setEducations(response.data.data);
       } catch (error) {
@@ -38,54 +36,100 @@ const Personal = ({ nextStep }) => {
   }, []);
 
   const handleEducationChange = (e) => {
-    setEdu(e.target.value)    
-    
+    const selectedEducationId = +e.target.value;
+    setPersonalList({...personalList, edu: selectedEducationId})
+    console.log(selectedEducationId);
   };
-  const handlePersonal = async (event)=>{
-    event.preventDefault()
-    try{
-      
-    setFirstname(firname);
-    setMiddlename(midname);
-    setLastname(lstname);
-    setAddress(addr);
-    setPhoneNum(phnnum);
-    setGender(gendr);
-    setEducation(edu);
-    }catch (error) {
-      console.error('Error:', error);
+  const handlePersonal = async (event) => {
+    
+    event.preventDefault();
+    const selectedEduObject = educations.find((educ) => educ.id === personalList.edu);
+    nextStep();
+    sessionStorage.setItem("personalListInfo", JSON.stringify({...personalList, edu: selectedEduObject.title}))
+    console.log(personalList);
 
   };
-  }
-  console.log("firstname:", firstname)
 
   // useEffect(() => {
   //   // This will log the updated firstname whenever it changes
   //   console.log("Updated firstname:", firstname);
-  // }, [firstname]); 
-
-  
+  // }, [firstname]);
 
   return (
     <div>
-      <div className="container" style={{ display: 'flex', flexDirection: 'column' }}>
+      <div
+        className="container"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
         <form onSubmit={handleSubmit}>
-          <input type="text" id="firstname" name="firstname" placeholder="First Name" required style={{ marginBottom: '10px' }} value={firname} onChange={(e) => setFirname(e.target.value)}/>
-          <input type="text" id="middlename" name="middlename" placeholder="Middle Name" style={{ marginBottom: '10px' }} value={midname}
-                  onChange={(e) => setMidname(e.target.value)} />
-          <input type="text" id="lastname" name="lastname" placeholder="Last Name" required style={{ marginBottom: '10px' }} value={lstname}
-                  onChange={(e) => setLstname(e.target.value)} />
-          <input type="text" id="homeaddress" name="homeaddress" placeholder="Home Address" style={{ marginBottom: '10px' }} value={addr}
-                  onChange={(e) => setAddr(e.target.value)} />
-          <input type="text" id="phonenumber" name="phonenumber" placeholder="Phone Number" required style={{ marginBottom: '10px' }} value={phnnum}
-                  onChange={(e) => setPhnnum(e.target.value)} />
-          <select id="Gender" name="Gender" style={{ marginBottom: '10px' }} required value={gendr}
-                  onChange={(e) => setGendr(e.target.value)}>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            placeholder="First Name"
+            required
+            style={{ marginBottom: "10px" }}
+            value={personalList.firname}
+            onChange={(e) => setPersonalList({...personalList, firname: e.target.value})}
+          />
+          <input
+            type="text"
+            id="middlename"
+            name="middlename"
+            placeholder="Middle Name"
+            style={{ marginBottom: "10px" }}
+            value={personalList.midname}
+            onChange={(e) => setPersonalList({...personalList, midname: e.target.value})}
+          />
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            placeholder="Last Name"
+            required
+            style={{ marginBottom: "10px" }}
+            value={personalList.lstname}
+            onChange={(e) => setPersonalList({...personalList, lstname: e.target.value})}
+          />
+          <input
+            type="text"
+            id="homeaddress"
+            name="homeaddress"
+            placeholder="Home Address"
+            style={{ marginBottom: "10px" }}
+            value={personalList.addr}
+            onChange={(e) => setPersonalList({...personalList, addr: e.target.value})}
+          />
+          <input
+            type="text"
+            id="phonenumber"
+            name="phonenumber"
+            placeholder="Phone Number"
+            required
+            style={{ marginBottom: "10px" }}
+            value={personalList.phnnum}
+            onChange={(e) => setPersonalList({...personalList, phnnum: e.target.value})}
+          />
+          <select
+            id="Gender"
+            name="Gender"
+            style={{ marginBottom: "10px" }}
+            required
+            value={personalList.gendr}
+            onChange={(e) => setPersonalList({...personalList, gendr: e.target.value})}
+          >
             <option value="">Select Gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
-          <select id="selectLocation" name="selectLocation" onChange={handleEducationChange} style={{ marginBottom: '10px' }} required value={edu}>
+          <select
+            id="selectLocation"
+            name="selectLocation"
+            onChange={handleEducationChange}
+            style={{ marginBottom: "10px" }}
+            required
+            value={personalList.edu}
+          >
             <option value="">Select Qualification</option>
             {educations.map((title) => (
               <option key={title.id} value={title.id}>
@@ -94,7 +138,21 @@ const Personal = ({ nextStep }) => {
             ))}
           </select>
 
-          <button type="submit" style={{ width: '100%', padding: '10px 20px', backgroundColor: '#502ef1', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }} onClick={handlePersonal}>NEXT</button>
+          <button
+            type="submit"
+            style={{
+              width: "100%",
+              padding: "10px 20px",
+              backgroundColor: "#502ef1",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+            onClick={handlePersonal}
+          >
+            NEXT
+          </button>
         </form>
       </div>
 
