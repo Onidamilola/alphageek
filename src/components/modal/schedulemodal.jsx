ebd4fb3432c6217e81e848c6775f94d9b6517607
Damilola Oni <damilolaoni46@gmail.com>
2024-04-08 16:06:30 +0100

    worked on the visit schedule

--- src/components/modal/schedulemodal.jsx
+++ src/components/modal/schedulemodal.jsx
@@ -10,14 +10,6 @@ const scheduleModal = () => {
    
   
 
-//   const outlett = outlets.outlet_id
-// console.log("helloooo:", outlets)
-//   console.log("the outlet from modal: ", outlets);
-
-
-  
-
-//   const formattedOutlet = { ...(editedOutlet || outlets) };
 
   return (
     <div className=" w-full bg-gray-100 p-6 rounded-t-3xl">
@@ -30,11 +22,11 @@ const scheduleModal = () => {
             <p>phone</p>
           </div>
         </div>
-        <div style={{ display: 'flex', justifyContent: 'flex-end' }}> {/* Added flexbox styles */}
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}> 
           <button
             type="submit"
             style={{
-              width: '100%', // Button width set to 100%
+              width: '100%', 
               padding: '10px 20px',
               backgroundColor: '#007bff',
               color: '#fff',
