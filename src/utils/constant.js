d226a87e859fd18a59904438c5744d1a129fecdb
Damilola Oni <damilolaoni46@gmail.com>
2024-05-03 11:55:35 +0100

    made changes to the register

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,7 +4,7 @@ let path = "https://d-aggregate.com/Alphageekbackend/api";
 export const LOGIN = `/login`;
 
 // login api, a post request
-export const REGISTER = `/create-user`; 
+export const REGISTER = `${path}/create-user`; 
 
 // login api, a post request
 export const VERIFY_CODE = `/verifycode`; 
